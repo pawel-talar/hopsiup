2bab469357842a4ac75a579f2f5ba4f83014d020
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2015-12-20 17:42:04 +0100

    Fix a bug in schema.sql

--- hopsiup/schema.sql
+++ hopsiup/schema.sql
@@ -8,20 +8,22 @@ create table users (
     login text not null unique,
     password text not null,
     info text not null,
-    points interger not null
+    points integer not null
 );
 
 create table links (
     id integer primary key autoincrement,
     link text not null,
     title text not null,
-    foreign key(user) references users(id)
+    user_id integer,
+    foreign key(user_id) references users(id)
 );
 
 create table comments (
     id integer primary key autoincrement,
-    foreign key(link_id) references links(id),
-    points integer not null
+    link_id integer,
+    points integer not null,
+    foreign key(link_id) references links(id)
 );
 
 create table tags (
