52b8d8ec95d4109f0fe270dfb4672558a806edb5
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2015-12-20 18:14:32 +0100

    Add default values and change names

--- hopsiup/schema.sql
+++ hopsiup/schema.sql
@@ -4,29 +4,30 @@ drop table if exists comments;
 drop table if exists tags;
 
 create table users (
-    id integer primary key autoincrement,
+    user_id integer primary key autoincrement,
     login text not null unique,
     password text not null,
     info text not null,
-    points integer not null
+    upoints integer not null default(0)
 );
 
 create table links (
-    id integer primary key autoincrement,
+    link_id integer primary key autoincrement,
     link text not null,
     title text not null,
-    user_id integer,
-    foreign key(user_id) references users(id)
+    user_id integer not null,
+    lpoints integer not null default(0),
+    foreign key(user_id) references users(user_id)
 );
 
 create table comments (
-    id integer primary key autoincrement,
+    comment_id integer primary key autoincrement,
     link_id integer,
-    points integer not null,
-    foreign key(link_id) references links(id)
+    cpoints integer not null default(0),
+    foreign key(link_id) references links(link_id)
 );
 
 create table tags (
-    id integer primary key autoincrement,
+    tag_id integer primary key autoincrement,
     name text not null
 );
